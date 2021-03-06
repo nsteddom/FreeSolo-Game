c27dae4debfddeda25ff32e581787545d612eb41
Nathan Steddom <nsteddom@utexas.edu>
2021-03-06 17:22:28 -0600

    staged_changes

--- state0.js
+++ state0.js
@@ -1,7 +1,7 @@
 // Level One
 // climbs on wall, height bar, sound, music (track sources), one piece of animated art
 
-var demo = {}, speed = 5, heightClimbed=0, text;
+var demo = {}, speed = 5, heightClimbed=0, text, map, layer;
 
 demo.state0 = function(){};
 
@@ -13,15 +13,17 @@ demo.state0.prototype = {
         // Images, Sprites, and Sounds to be used in this scene. 
         game.load.spritesheet('baddie', 'assets/baddie.png', 32, 32);
         //game.load.image('mountain', 'assets/');
-        game.load.image('sky', 'assets/sky.png');
-        game.load.image('ground', 'assets/platform.png');
+        // game.load.image('sky', 'assets/sky.png');
+        // game.load.image('ground', 'assets/platform.png');
         game.load.spritesheet('baddie', 'assets/baddie.png', 32, 32);
         game.load.image('star', 'assets/star.png');
-        game.load.audio('music', 'assets/speck_-_Drum_n_Bird_03_(The_Crowening).mp3');
-        game.load.audio('soundeffect', 'assets/mixkit-meadow-wind-light-1166.wav')
+        game.load.audio('music', 'assets/sounds/speck_-_Drum_n_Bird_03_(The_Crowening).mp3');
+        game.load.audio('soundeffect', 'assets/sounds/mixkit-meadow-wind-light-1166.wav');
         game.load.image('water','assets/Bottle.png');
-        game.load.image('mountain', 'assets/mountain.png', 800, 400);
-
+        // game.load.image('mountain', 'assets/mountain.png', 800, 400);
+        game.load.image('bird', 'assets/Blackbird.gif');
+        game.load.tilemap('base', 'assets/tiles/base.json', null, Phaser.Tilemap.TILED_JSON);
+        game.load.image('rockTile1', 'assets/tiles/rockTile1.png')
 
 
 
@@ -29,21 +31,32 @@ demo.state0.prototype = {
 
 
     create: function(){
+        map = game.add.tilemap('base')
+        map.addTilesetImage('rockTile1', 'rockTile1');
+        layer = map.createLayer('Scene1');
+
+
+
+
+
         
         game.physics.startSystem(Phaser.Physics.ARCADE)
 
         addChangeStateEventListeners();
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-        game.add.sprite(0, 0, 'sky');
-        game.add.sprite(200, 300, 'mountain');
-        game.add.sprite(200, 0, 'mountain');
+        // game.add.sprite(0, 0, 'sky');
+        // game.add.sprite(200, 300, 'mountain');
+        // game.add.sprite(200, 0, 'mountain');
         game.add.sprite(300, 200, 'water');
+        
 
         // Added star image in the game for animated aspect. 
         star = game.add.image(550, game.world.height - 300, 'star')
+        bird = game.add.image(550, game.world.height - 300, 'bird')
 
         
         player = game.add.sprite(300, game.world.height - 150, 'baddie')
+     
         game.physics.enable(player)
         // player.body.gravity.y = 500
         player.body.collideWorldBounds = true;  
@@ -52,8 +65,11 @@ demo.state0.prototype = {
 
         soundEffect = game.add.audio('soundeffect');
 
-        backgroundMusic.play();
-        soundEffect.play();
+        backgroundMusic.onDecoded.add(start, this);
+
+
+        // backgroundMusic.play();
+        // soundEffect.play();
 
 
         
@@ -82,7 +98,7 @@ demo.state0.prototype = {
 
         // Makes star go in a circle
         // star.angle +=3;
-        moveStar(star, 1);
+        moveStar(bird, 1);
 
         
         text.destroy();
@@ -152,9 +168,13 @@ function moveStar(star, speed) {
 
 function resetStarPos(star) {
     star.x = 0;
-    // var randomY = Math.between(0, game.world.height);
-    star.y = 300
+    var randomY = Math.random() * game.world.height;
+    star.y = randomY
     }
+
+function start() {
+    backgroundMusic.fadeIn(4000);
+}
 
 
 
