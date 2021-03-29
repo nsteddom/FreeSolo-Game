5c4434f4e9ab5eafcbfb315bd9a5ca111db1cb9f
Nathan Steddom <nsteddom@utexas.edu>
2021-03-28 21:31:26 -0500

    size back to 600

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(800, 576, Phaser.Auto)
+var game = new Phaser.Game(800, 600, Phaser.Auto)
 game.state.add('state0', demo.state0);
 game.state.add('state1', demo.state1);
 game.state.start('state1');
